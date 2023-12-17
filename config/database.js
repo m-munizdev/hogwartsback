68f10d33911a663bc4a23daa8d3c26b192f40099
Emerson Muniz <emersonmunizecp@gmail.com>
2023-12-17 18:43:57 -0300

    atualizado 2

--- config/database.js
+++ config/database.js
@@ -1,16 +1,10 @@
-const pg = require('pg');
-
-const pool = new pg.Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: 'artemisapolo2',
-  database: 'hogwarts'
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: "postgres://hogwarts_2dm2_user:aeOuaMfHhawQOxSwJssjpB6yH8dmHamm@dpg-clvm5rmd3nmc738ehlmg-a.oregon-postgres.render.com/hogwarts_2dm2",
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-
-module.exports = pool; 
-
-
-
+module.exports = pool;
 
 
